734815f204fcbbd6569cc788a8fe3db70905313b
benjholla <benjholla@gmail.com>
2015-01-19 11:41:55 -0600

    added support libraries and renamed constants

--- com.qc.libanalysis.toolbox/src/com/qc/adlib/android/AdLibrary.java
+++ com.qc.libanalysis.toolbox/src/com/qc/adlib/android/AdLibrary.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 import com.ensoftcorp.atlas.java.core.query.Q;
-import com.ensoftcorp.atlas.java.core.script.Common;
 import com.qc.adlib.PackageUtils;
 
 /**
@@ -45,7 +44,7 @@ public class AdLibrary {
 	 * @return
 	 */
 	public Q getPackages(){
-		return PackageUtils.getPackageWithSubpackages(Common.universe(), basePackage);
+		return PackageUtils.getPackageWithSubpackages(basePackage);
 	}
 	
 	/**
@@ -53,7 +52,15 @@ public class AdLibrary {
 	 * @return
 	 */
 	public Q getTypes(){
-		return PackageUtils.getPackageTypes(Common.universe(), getPackages());
+		return PackageUtils.getPackageTypes(getPackages());
+	}
+	
+	/**
+	 * Returns a Q containing all the declarations made in the packages contained in this advertisement library
+	 * @return
+	 */
+	public Q getDeclarations(){
+		return PackageUtils.getPackageDeclarations(basePackage);
 	}
 	
 	/**
@@ -64,72 +71,72 @@ public class AdLibrary {
 		return reference;
 	}
 	
-	public static final AdLibrary AdMobGoogle = new AdLibrary("AdMob (com.google)", "com.google.ads", null);
-	public static final AdLibrary Flurry = new AdLibrary("Flurry", "com.flurry", null);
-	public static final AdLibrary InMobi = new AdLibrary("InMobi", "com.inmobi", null);
-	public static final AdLibrary TapJoy = new AdLibrary("TapJoy", "com.tapjoy", null);
-	public static final AdLibrary MobClix = new AdLibrary("MobClix", "com.mobclix", null);
-	public static final AdLibrary ChartBoost = new AdLibrary("ChartBoost", "com.chartboost", null);
-	public static final AdLibrary AdWhirl = new AdLibrary("AdWhirl", "com.adwhirl", null);
-	public static final AdLibrary MoPub = new AdLibrary("MoPub", "com.mopub", null);
-	public static final AdLibrary GreyStripe = new AdLibrary("GreyStripe", "com.greystripe", null);
-	public static final AdLibrary JumpTap = new AdLibrary("JumpTap", "com.jumptap", null);
-	public static final AdLibrary GoogleAnalytics = new AdLibrary("Google Analytics", "com.google.analytics", null);
-	public static final AdLibrary AdMob = new AdLibrary("AdMob (com.admob)", "com.admob", null);
-	public static final AdLibrary Burstly = new AdLibrary("Burstly", "com.burstly", null);
-	public static final AdLibrary SponsorPay = new AdLibrary("SponsorPay", "com.sponsorpay", null);
-	public static final AdLibrary Cauly = new AdLibrary("Cauly", "com.cauly", null);
-	public static final AdLibrary MobFox = new AdLibrary("MobFox", "com.mobfox", null);
-	public static final AdLibrary Vpon = new AdLibrary("Vpon.com", "com.vpon", null);
-	public static final AdLibrary AppBrain = new AdLibrary("AppBrain", "com.appbrain", null);
-	public static final AdLibrary Daum = new AdLibrary("Daum.net", "net.daum", null);
-	public static final AdLibrary AdMarvel = new AdLibrary("AdMarvel", "com.admarvel", null);
-	public static final AdLibrary AppLovin = new AdLibrary("AppLovin", "com.applovin", null);
-	public static final AdLibrary Adfonic = new AdLibrary("Adfonic", "com.adfonic", null);
-	public static final AdLibrary MdotM = new AdLibrary("MdotM", "com.mdotm", null);
-	public static final AdLibrary GetJar = new AdLibrary("GetJar", "com.getjar", null);
-	public static final AdLibrary NexAge = new AdLibrary("NexAge", "com.nexage", null);
-	public static final AdLibrary InnerActive = new AdLibrary("InnerActive", "com.inneractive", null);
-	public static final AdLibrary Pontiflex = new AdLibrary("Pontiflex", "com.pontiflex", null);
-	public static final AdLibrary ZestAdz = new AdLibrary("ZestAdz", "com.zestadz", null);
-	public static final AdLibrary MadHouse = new AdLibrary("MadHouse", "com.madhouse", null);
-	public static final AdLibrary Smaato = new AdLibrary("Smaato", "com.smaato", null);
-	public static final AdLibrary YouMi = new AdLibrary("YouMi", "net.youmi", null);
-	public static final AdLibrary mAdvertiseDE = new AdLibrary("mAdvertise.de", "de.madvertise", null);
-	public static final AdLibrary DoMobCN = new AdLibrary("DoMob.cn", "cn.domob", null);
-	public static final AdLibrary Jirbo_AdColony = new AdLibrary("Jirbo/AdColony", "com.jirbo.adcolony", null);
-	public static final AdLibrary RevMob = new AdLibrary("RevMob", "com.revmob", null);
-	public static final AdLibrary SendDroid = new AdLibrary("SendDroid", "com.senddroid", null);
-	public static final AdLibrary AirPush = new AdLibrary("AirPush", "com.airpush", null);
-	public static final AdLibrary Tapit = new AdLibrary("Tapit", "com.tapit", null);
-	public static final AdLibrary Medialets = new AdLibrary("Medialets", "com.medialets", null);
-	public static final AdLibrary Mediba = new AdLibrary("Mediba", "mediba.ad", null);
-	public static final AdLibrary Papaya = new AdLibrary("Papaya", "com.papaya", null);
-	public static final AdLibrary HuntMobileAds = new AdLibrary("Hunt Mobile Ads", "com.huntmads", null);
-	public static final AdLibrary RhythmNewMedia = new AdLibrary("RhythmNewMedia", "com.rhythmnewmedia", null);
-	public static final AdLibrary TapForTap = new AdLibrary("TapForTap", "com.tapfortap", null);
-	public static final AdLibrary AdKnowledge = new AdLibrary("AdKnowledge", "com.adknowledge", null);
-	public static final AdLibrary Metaps = new AdLibrary("Metaps", "net.metaps", null);
-	public static final AdLibrary WiYun = new AdLibrary("WiYun", "com.wiyun", null);
-	public static final AdLibrary Vdopia = new AdLibrary("Vdopia.com", "com.vdopia", null);
-	public static final AdLibrary Waps = new AdLibrary("Waps", "com.waps", null);
-	public static final AdLibrary AdWo = new AdLibrary("AdWo", "com.adwo", null);
-	public static final AdLibrary MoboSquare = new AdLibrary("MoboSquare", "com.mobosquare", null);
-	public static final AdLibrary Vserv = new AdLibrary("Vserv", "mobi.vserv", null);
-	public static final AdLibrary WooBoo = new AdLibrary("WooBoo", "com.wooboo", null);
-	public static final AdLibrary EverBadge = new AdLibrary("EverBadge", "com.everbadge", null);
-	public static final AdLibrary AirAd = new AdLibrary("AirAd", "com.mt.airad", null);
-	public static final AdLibrary Noqoush_AdFalcon = new AdLibrary("Noqoush/AdFalcon", "com.noqoush.adfalcon", null);
-	public static final AdLibrary Moolah = new AdLibrary("Moolah", "com.moolah", null);
-	public static final AdLibrary Kuguo = new AdLibrary("Kuguo", "com.kuguo", null);
-	public static final AdLibrary BuzzCity = new AdLibrary("BuzzCity", "buzzcity", null);
-	public static final AdLibrary AdsMogo = new AdLibrary("AdsMogo", "com.adsmogo", null);
-	public static final AdLibrary SellaRing = new AdLibrary("SellaRing", "com.sellaring", null);
-	public static final AdLibrary StartApp = new AdLibrary("StartApp", "com.startapp", null);
-	public static final AdLibrary AdModa = new AdLibrary("AdModa", "com.admoda", null);
-	public static final AdLibrary MobPartner = new AdLibrary("MobPartner", "com.mobpartner", null);
-	public static final AdLibrary QuClix = new AdLibrary("QuClix", "com.quclix", null);
-	public static final AdLibrary lDevelop = new AdLibrary("lDevelop", "com.ldevelop", null);
+	public static final AdLibrary ADMOB_GOOGLE = new AdLibrary("AdMob (com.google)", "com.google.ads", null);
+	public static final AdLibrary FLURRY = new AdLibrary("Flurry", "com.flurry", null);
+	public static final AdLibrary IN_MOBI = new AdLibrary("InMobi", "com.inmobi", null);
+	public static final AdLibrary TAP_JOY = new AdLibrary("TapJoy", "com.tapjoy", null);
+	public static final AdLibrary MOB_CLIX = new AdLibrary("MobClix", "com.mobclix", null);
+	public static final AdLibrary CHART_BOOST = new AdLibrary("ChartBoost", "com.chartboost", null);
+	public static final AdLibrary AD_WHIRL = new AdLibrary("AdWhirl", "com.adwhirl", null);
+	public static final AdLibrary MO_PUB = new AdLibrary("MoPub", "com.mopub", null);
+	public static final AdLibrary GREY_STRIPE = new AdLibrary("GreyStripe", "com.greystripe", null);
+	public static final AdLibrary JUMP_TAP = new AdLibrary("JumpTap", "com.jumptap", null);
+	public static final AdLibrary GOOGLE_ANALYTICS = new AdLibrary("Google Analytics", "com.google.analytics", null);
+	public static final AdLibrary ADMOB = new AdLibrary("AdMob (com.admob)", "com.admob", null);
+	public static final AdLibrary BURSTLY = new AdLibrary("Burstly", "com.burstly", null);
+	public static final AdLibrary SPONSOR_PAY = new AdLibrary("SponsorPay", "com.sponsorpay", null);
+	public static final AdLibrary CAULY = new AdLibrary("Cauly", "com.cauly", null);
+	public static final AdLibrary MOB_FOX = new AdLibrary("MobFox", "com.mobfox", null);
+	public static final AdLibrary VPON = new AdLibrary("Vpon.com", "com.vpon", null);
+	public static final AdLibrary APP_BRAIN = new AdLibrary("AppBrain", "com.appbrain", null);
+	public static final AdLibrary DAUM = new AdLibrary("Daum.net", "net.daum", null);
+	public static final AdLibrary AD_MARVEL = new AdLibrary("AdMarvel", "com.admarvel", null);
+	public static final AdLibrary APP_LOVIN = new AdLibrary("AppLovin", "com.applovin", null);
+	public static final AdLibrary ADFONIC = new AdLibrary("Adfonic", "com.adfonic", null);
+	public static final AdLibrary MDOT_M = new AdLibrary("MdotM", "com.mdotm", null);
+	public static final AdLibrary GET_JAR = new AdLibrary("GetJar", "com.getjar", null);
+	public static final AdLibrary NEX_AGE = new AdLibrary("NexAge", "com.nexage", null);
+	public static final AdLibrary INNER_ACTIVE = new AdLibrary("InnerActive", "com.inneractive", null);
+	public static final AdLibrary PONTIFLEX = new AdLibrary("Pontiflex", "com.pontiflex", null);
+	public static final AdLibrary ZEST_ADZ = new AdLibrary("ZestAdz", "com.zestadz", null);
+	public static final AdLibrary MAD_HOUSE = new AdLibrary("MadHouse", "com.madhouse", null);
+	public static final AdLibrary SMAATO = new AdLibrary("Smaato", "com.smaato", null);
+	public static final AdLibrary YOU_MI = new AdLibrary("YouMi", "net.youmi", null);
+	public static final AdLibrary M_ADVERTISE_DE = new AdLibrary("mAdvertise.de", "de.madvertise", null);
+	public static final AdLibrary DO_MOB_CN = new AdLibrary("DoMob.cn", "cn.domob", null);
+	public static final AdLibrary JIRBO_AD_COLONY = new AdLibrary("Jirbo/AdColony", "com.jirbo.adcolony", null);
+	public static final AdLibrary REV_MOB = new AdLibrary("RevMob", "com.revmob", null);
+	public static final AdLibrary SEND_DROID = new AdLibrary("SendDroid", "com.senddroid", null);
+	public static final AdLibrary AIR_PUSH = new AdLibrary("AirPush", "com.airpush", null);
+	public static final AdLibrary TAPIT = new AdLibrary("Tapit", "com.tapit", null);
+	public static final AdLibrary MEDIALETS = new AdLibrary("Medialets", "com.medialets", null);
+	public static final AdLibrary MEDIBA = new AdLibrary("Mediba", "mediba.ad", null);
+	public static final AdLibrary PAPAYA = new AdLibrary("Papaya", "com.papaya", null);
+	public static final AdLibrary HUNT_MOBILE_ADS = new AdLibrary("Hunt Mobile Ads", "com.huntmads", null);
+	public static final AdLibrary RHYTHM_NEW_MEDIA = new AdLibrary("RhythmNewMedia", "com.rhythmnewmedia", null);
+	public static final AdLibrary TAP_FOR_TAP = new AdLibrary("TapForTap", "com.tapfortap", null);
+	public static final AdLibrary AD_KNOWLEDGE = new AdLibrary("AdKnowledge", "com.adknowledge", null);
+	public static final AdLibrary METAPS = new AdLibrary("Metaps", "net.metaps", null);
+	public static final AdLibrary WI_YUN = new AdLibrary("WiYun", "com.wiyun", null);
+	public static final AdLibrary VDOPIA = new AdLibrary("Vdopia.com", "com.vdopia", null);
+	public static final AdLibrary WAPS = new AdLibrary("Waps", "com.waps", null);
+	public static final AdLibrary AD_WO = new AdLibrary("AdWo", "com.adwo", null);
+	public static final AdLibrary MOBO_SQUARE = new AdLibrary("MoboSquare", "com.mobosquare", null);
+	public static final AdLibrary VSERV = new AdLibrary("Vserv", "mobi.vserv", null);
+	public static final AdLibrary WOO_BOO = new AdLibrary("WooBoo", "com.wooboo", null);
+	public static final AdLibrary EVER_BADGE = new AdLibrary("EverBadge", "com.everbadge", null);
+	public static final AdLibrary AIR_AD = new AdLibrary("AirAd", "com.mt.airad", null);
+	public static final AdLibrary NOQOUSH_AD_FALCON = new AdLibrary("Noqoush/AdFalcon", "com.noqoush.adfalcon", null);
+	public static final AdLibrary MOOLAH = new AdLibrary("Moolah", "com.moolah", null);
+	public static final AdLibrary KUGUO = new AdLibrary("Kuguo", "com.kuguo", null);
+	public static final AdLibrary BUZZ_CITY = new AdLibrary("BuzzCity", "buzzcity", null);
+	public static final AdLibrary ADS_MOGO = new AdLibrary("AdsMogo", "com.adsmogo", null);
+	public static final AdLibrary SELLA_RING = new AdLibrary("SellaRing", "com.sellaring", null);
+	public static final AdLibrary START_APP = new AdLibrary("StartApp", "com.startapp", null);
+	public static final AdLibrary ADMODA = new AdLibrary("AdModa", "com.admoda", null);
+	public static final AdLibrary MOB_PARTNER = new AdLibrary("MobPartner", "com.mobpartner", null);
+	public static final AdLibrary QU_CLIX = new AdLibrary("QuClix", "com.quclix", null);
+	public static final AdLibrary L_DEVELOP = new AdLibrary("lDevelop", "com.ldevelop", null);
 	
 	/**
 	 * Returns a collection of all known Android advertisement libraries
@@ -137,72 +144,72 @@ public class AdLibrary {
 	 */
 	public static Collection<AdLibrary> getAllAdvertisementLibraries(){
 		Collection<AdLibrary> allAdvertisementLibraries = new LinkedList<AdLibrary>();
-		allAdvertisementLibraries.add(AdMobGoogle);
-		allAdvertisementLibraries.add(Flurry);
-		allAdvertisementLibraries.add(InMobi);
-		allAdvertisementLibraries.add(TapJoy);
-		allAdvertisementLibraries.add(MobClix);
-		allAdvertisementLibraries.add(ChartBoost);
-		allAdvertisementLibraries.add(AdWhirl);
-		allAdvertisementLibraries.add(MoPub);
-		allAdvertisementLibraries.add(GreyStripe);
-		allAdvertisementLibraries.add(JumpTap);
-		allAdvertisementLibraries.add(GoogleAnalytics);
-		allAdvertisementLibraries.add(AdMob);
-		allAdvertisementLibraries.add(Burstly);
-		allAdvertisementLibraries.add(SponsorPay);
-		allAdvertisementLibraries.add(Cauly);
-		allAdvertisementLibraries.add(MobFox);
-		allAdvertisementLibraries.add(Vpon);
-		allAdvertisementLibraries.add(AppBrain);
-		allAdvertisementLibraries.add(Daum);
-		allAdvertisementLibraries.add(AdMarvel);
-		allAdvertisementLibraries.add(AppLovin);
-		allAdvertisementLibraries.add(Adfonic);
-		allAdvertisementLibraries.add(MdotM);
-		allAdvertisementLibraries.add(GetJar);
-		allAdvertisementLibraries.add(NexAge);
-		allAdvertisementLibraries.add(InnerActive);
-		allAdvertisementLibraries.add(Pontiflex);
-		allAdvertisementLibraries.add(ZestAdz);
-		allAdvertisementLibraries.add(MadHouse);
-		allAdvertisementLibraries.add(Smaato);
-		allAdvertisementLibraries.add(YouMi);
-		allAdvertisementLibraries.add(mAdvertiseDE);
-		allAdvertisementLibraries.add(DoMobCN);
-		allAdvertisementLibraries.add(Jirbo_AdColony);
-		allAdvertisementLibraries.add(RevMob);
-		allAdvertisementLibraries.add(SendDroid);
-		allAdvertisementLibraries.add(AirPush);
-		allAdvertisementLibraries.add(Tapit);
-		allAdvertisementLibraries.add(Medialets);
-		allAdvertisementLibraries.add(Mediba);
-		allAdvertisementLibraries.add(Papaya);
-		allAdvertisementLibraries.add(HuntMobileAds);
-		allAdvertisementLibraries.add(RhythmNewMedia);
-		allAdvertisementLibraries.add(TapForTap);
-		allAdvertisementLibraries.add(AdKnowledge);
-		allAdvertisementLibraries.add(Metaps);
-		allAdvertisementLibraries.add(WiYun);
-		allAdvertisementLibraries.add(Vdopia);
-		allAdvertisementLibraries.add(Waps);
-		allAdvertisementLibraries.add(AdWo);
-		allAdvertisementLibraries.add(MoboSquare);
-		allAdvertisementLibraries.add(Vserv);
-		allAdvertisementLibraries.add(WooBoo);
-		allAdvertisementLibraries.add(EverBadge);
-		allAdvertisementLibraries.add(AirAd);
-		allAdvertisementLibraries.add(Noqoush_AdFalcon);
-		allAdvertisementLibraries.add(Moolah);
-		allAdvertisementLibraries.add(Kuguo);
-		allAdvertisementLibraries.add(BuzzCity);
-		allAdvertisementLibraries.add(AdsMogo);
-		allAdvertisementLibraries.add(SellaRing);
-		allAdvertisementLibraries.add(StartApp);
-		allAdvertisementLibraries.add(AdModa);
-		allAdvertisementLibraries.add(MobPartner);
-		allAdvertisementLibraries.add(QuClix);
-		allAdvertisementLibraries.add(lDevelop);
+		allAdvertisementLibraries.add(ADMOB_GOOGLE);
+		allAdvertisementLibraries.add(FLURRY);
+		allAdvertisementLibraries.add(IN_MOBI);
+		allAdvertisementLibraries.add(TAP_JOY);
+		allAdvertisementLibraries.add(MOB_CLIX);
+		allAdvertisementLibraries.add(CHART_BOOST);
+		allAdvertisementLibraries.add(AD_WHIRL);
+		allAdvertisementLibraries.add(MO_PUB);
+		allAdvertisementLibraries.add(GREY_STRIPE);
+		allAdvertisementLibraries.add(JUMP_TAP);
+		allAdvertisementLibraries.add(GOOGLE_ANALYTICS);
+		allAdvertisementLibraries.add(ADMOB);
+		allAdvertisementLibraries.add(BURSTLY);
+		allAdvertisementLibraries.add(SPONSOR_PAY);
+		allAdvertisementLibraries.add(CAULY);
+		allAdvertisementLibraries.add(MOB_FOX);
+		allAdvertisementLibraries.add(VPON);
+		allAdvertisementLibraries.add(APP_BRAIN);
+		allAdvertisementLibraries.add(DAUM);
+		allAdvertisementLibraries.add(AD_MARVEL);
+		allAdvertisementLibraries.add(APP_LOVIN);
+		allAdvertisementLibraries.add(ADFONIC);
+		allAdvertisementLibraries.add(MDOT_M);
+		allAdvertisementLibraries.add(GET_JAR);
+		allAdvertisementLibraries.add(NEX_AGE);
+		allAdvertisementLibraries.add(INNER_ACTIVE);
+		allAdvertisementLibraries.add(PONTIFLEX);
+		allAdvertisementLibraries.add(ZEST_ADZ);
+		allAdvertisementLibraries.add(MAD_HOUSE);
+		allAdvertisementLibraries.add(SMAATO);
+		allAdvertisementLibraries.add(YOU_MI);
+		allAdvertisementLibraries.add(M_ADVERTISE_DE);
+		allAdvertisementLibraries.add(DO_MOB_CN);
+		allAdvertisementLibraries.add(JIRBO_AD_COLONY);
+		allAdvertisementLibraries.add(REV_MOB);
+		allAdvertisementLibraries.add(SEND_DROID);
+		allAdvertisementLibraries.add(AIR_PUSH);
+		allAdvertisementLibraries.add(TAPIT);
+		allAdvertisementLibraries.add(MEDIALETS);
+		allAdvertisementLibraries.add(MEDIBA);
+		allAdvertisementLibraries.add(PAPAYA);
+		allAdvertisementLibraries.add(HUNT_MOBILE_ADS);
+		allAdvertisementLibraries.add(RHYTHM_NEW_MEDIA);
+		allAdvertisementLibraries.add(TAP_FOR_TAP);
+		allAdvertisementLibraries.add(AD_KNOWLEDGE);
+		allAdvertisementLibraries.add(METAPS);
+		allAdvertisementLibraries.add(WI_YUN);
+		allAdvertisementLibraries.add(VDOPIA);
+		allAdvertisementLibraries.add(WAPS);
+		allAdvertisementLibraries.add(AD_WO);
+		allAdvertisementLibraries.add(MOBO_SQUARE);
+		allAdvertisementLibraries.add(VSERV);
+		allAdvertisementLibraries.add(WOO_BOO);
+		allAdvertisementLibraries.add(EVER_BADGE);
+		allAdvertisementLibraries.add(AIR_AD);
+		allAdvertisementLibraries.add(NOQOUSH_AD_FALCON);
+		allAdvertisementLibraries.add(MOOLAH);
+		allAdvertisementLibraries.add(KUGUO);
+		allAdvertisementLibraries.add(BUZZ_CITY);
+		allAdvertisementLibraries.add(ADS_MOGO);
+		allAdvertisementLibraries.add(SELLA_RING);
+		allAdvertisementLibraries.add(START_APP);
+		allAdvertisementLibraries.add(ADMODA);
+		allAdvertisementLibraries.add(MOB_PARTNER);
+		allAdvertisementLibraries.add(QU_CLIX);
+		allAdvertisementLibraries.add(L_DEVELOP);
 		return allAdvertisementLibraries;
 	}
 	
--- com.qc.libanalysis.toolbox/src/com/qc/adlib/android/SupportLibrary.java
+++ com.qc.libanalysis.toolbox/src/com/qc/adlib/android/SupportLibrary.java
@@ -1,5 +1,90 @@
 package com.qc.adlib.android;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
+import com.ensoftcorp.atlas.java.core.query.Q;
+import com.qc.adlib.PackageUtils;
+
+/**
+ * A collection of known Android support libraries and their properties
+ * 
+ * @author Ben Holland
+ */
 public class SupportLibrary {
 
+	private String name;
+	private String basePackage;
+	private String reference;
+
+	public SupportLibrary(String name, String basePackage, String reference) {
+		this.name = name;
+		this.basePackage = basePackage;
+		this.reference = reference;
+	}
+
+	/**
+	 * Returns the support library name
+	 * 
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the base package of the support library
+	 * 
+	 * @return
+	 */
+	public String getBasePackage() {
+		return basePackage;
+	}
+
+	/**
+	 * Returns a Q containing the packages contained in this support library
+	 * @return
+	 */
+	public Q getPackages(){
+		return PackageUtils.getPackageWithSubpackages(basePackage);
+	}
+	
+	/**
+	 * Returns a Q containing the types contained in the packages contained in this support library
+	 * @return
+	 */
+	public Q getTypes(){
+		return PackageUtils.getPackageTypes(getPackages());
+	}
+	
+	/**
+	 * Returns a Q containing all the declarations made in the packages contained in this support library
+	 * @return
+	 */
+	public Q getDeclarations(){
+		return PackageUtils.getPackageDeclarations(basePackage);
+	}
+
+	/**
+	 * Returns a reference to the advertisement library or null if none exists
+	 * 
+	 * @return
+	 */
+	public String getReference() {
+		return reference;
+	}
+	
+	public static final SupportLibrary APP_COMPAT_V4 = new SupportLibrary("Android Support Library v4", "android.support.v4", "https://developer.android.com/tools/support-library/index.html");
+	public static final SupportLibrary APP_COMPAT_V7 = new SupportLibrary("Android Support Library v7", "android.support.v7", "https://developer.android.com/tools/support-library/index.html");
+
+	/**
+	 * Returns a collection of all known Android support libraries
+	 * @return
+	 */
+	public static Collection<SupportLibrary> getAllAdvertisementLibraries(){
+		Collection<SupportLibrary> allSupportLibraries = new LinkedList<SupportLibrary>();
+		allSupportLibraries.add(APP_COMPAT_V4);
+		allSupportLibraries.add(APP_COMPAT_V7);
+		return allSupportLibraries;
+	}
 }
